f25e70d950229078e27903de16fdf6a9adeb9514
fusellifrancesco <fusellifrancesco05@gmail.com>
2023-01-22 22:45:01 +0100

    Aggiunto Data Annotation Stringlength a Pizza e eseguita terza migrazione

---
+++ La mia pizzeria ef post/Migrations/20230122214153_StringLengthErrorMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lamiapizzeriaefpost.Migrations
+{
+    /// <inheritdoc />
+    public partial class StringLengthErrorMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- La mia pizzeria ef post/Models/Pizza.cs
+++ La mia pizzeria ef post/Models/Pizza.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NetCore_01.Models {
@@ -6,12 +7,14 @@ namespace NetCore_01.Models {
         public int Id { get; set; }
 
         [Column(TypeName = "varchar(100)")]
+        [StringLength(100, ErrorMessage = "Il campo nome non può contenere più di 100 caratteri")]
         public string Name { get; set; }
 
         [Column(TypeName = "text")]
         public string Description { get; set; }
         
         [Column(TypeName = "varchar(300)")]
+        [StringLength(300, ErrorMessage = "Il campo immagine non può contenere più di 300 caratteri")]
         public string Img { get; set; }
 
         public Pizza() { 
