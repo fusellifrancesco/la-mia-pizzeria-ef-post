d992b64dbbcab1620483996e10624496266d4ec1
fusellifrancesco <fusellifrancesco05@gmail.com>
2023-01-22 21:38:16 +0100

    Aggiunti metodi Index, Details, Create su PizzaController

--- La mia pizzeria ef post/Controllers/PizzaController.cs
+++ La mia pizzeria ef post/Controllers/PizzaController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using La_mia_pizzeria_ef_post.Database;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.JSInterop.Infrastructure;
 using NetCore_01.Models;
 
@@ -6,8 +7,56 @@ namespace La_mia_pizzeria_ef_post.Controllers {
     public class PizzaController : Controller {
 
         public IActionResult Index() {
-            return View();
+            
+            using(PizzaContext db = new PizzaContext()) {
+                
+                List<Pizza> ListaPizze = db.Pizze.ToList<Pizza>();
+
+                return View("Index", ListaPizze);
+            }
+            
         }
-        
+
+        public IActionResult Details(int id) {
+
+            using (PizzaContext db = new PizzaContext()) {
+
+                Pizza PizzaTrovata = db.Pizze
+                    .Where(SingolaPizzaNelDb => SingolaPizzaNelDb.Id == id)
+                    .FirstOrDefault();
+
+                if (PizzaTrovata != null) {
+                    
+                    return View(PizzaTrovata);
+                }
+
+                return NotFound("La pizza con l'id cercato non esiste");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create() {
+            
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Pizza PizzaData) {
+
+            if (!ModelState.IsValid) {
+
+                return View("Create", PizzaData);
+
+            }
+
+            using (PizzaContext db = new PizzaContext()) {
+
+                db.Pizze.Add(PizzaData);
+                db.SaveChanges();
+
+            }
+
+            return RedirectToAction("Index");
+        }   
     }
 }
