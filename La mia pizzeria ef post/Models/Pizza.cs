9f75f1575c3ccfac861fc5b605ffb75b207e8550
fusellifrancesco <fusellifrancesco05@gmail.com>
2023-01-22 20:19:17 +0100

    Add project files.

---
+++ La mia pizzeria ef post/Models/Pizza.cs
@@ -0,0 +1,18 @@
+﻿namespace NetCore_01.Models {
+    public class Pizza {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Img { get; }
+
+        public Pizza(int id, string name, string description, string img) {
+            Id = id;
+            Name = name;
+            Description = description;
+            Img = img;
+        }
+    }
+
+}
+
+
