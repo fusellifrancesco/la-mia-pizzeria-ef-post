b8b98e14d56e357da840e148ded5103f31856039
fusellifrancesco <fusellifrancesco05@gmail.com>
2023-01-22 21:10:41 +0100

    Correzzione errori su controller, Completato PizzaContext e eseguita prima migration

--- La mia pizzeria ef post/Controllers/PizzaController.cs
+++ La mia pizzeria ef post/Controllers/PizzaController.cs
@@ -1,27 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.JSInterop.Infrastructure;
 using NetCore_01.Models;
-using La_mia_pizzeria_Model.Utils;
 
-namespace La_mia_pizzeria_Model.Controllers {
+namespace La_mia_pizzeria_ef_post.Controllers {
     public class PizzaController : Controller {
 
         public IActionResult Index() {
-
-            List<Pizza> ListaPizze = PizzaData.GetPizze();
-
-            return View("Index", ListaPizze);
+            return View();
         }
-
-        public IActionResult Details(int id) {
-            List<Pizza> ListaPizze = PizzaData.GetPizze();
-
-            foreach (Pizza pizze in ListaPizze) {
-                if (pizze.Id == id) {
-                    return View(pizze);
-                }
-            }
-
-            return NotFound("La pizza con l'id cercato non esiste!");
-        }
+        
     }
 }
--- La mia pizzeria ef post/Database/PizzaContext.cs
+++ La mia pizzeria ef post/Database/PizzaContext.cs
@@ -1,4 +1,15 @@
-﻿namespace La_mia_pizzeria_ef_post.Database {
-    public class PizzaContext {
+﻿using Microsoft.EntityFrameworkCore;
+using NetCore_01.Models;
+
+namespace La_mia_pizzeria_ef_post.Database {
+    public class PizzaContext : DbContext {
+
+        public DbSet<Pizza> Pizze { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Database=MiaPizzeriaEF;" +
+            "Integrated Security=True;TrustServerCertificate=True");
+        }
+
     }
 }
